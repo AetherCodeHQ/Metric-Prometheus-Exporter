7b5daccc73d4bdd339a029e8c749200cc04c4115
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:58:07 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
-
 package main
 
 import (
 	"fmt"
-	"net"
+	"net/http"
 	"os"
-	"strconv"
-	"strings"
+	"runtime"
 	"time"
 )
 
 func main() {
-	host := "127.0.0.1"
-	ports := []int{22, 80, 443, 8080}
+	port := "9100"
 	if len(os.Args) > 1 {
-		host = os.Args[1]
+		port = os.Args[1]
 	}
-	if len(os.Args) > 2 {
-		ports = ports[:0]
-		for _, q := range strings.Split(os.Args[2], ",") {
-			if n, err := strconv.Atoi(q); err == nil {
-				ports = append(ports, n)
-			}
-		}
-	}
-	for _, p := range ports {
-		addr := fmt.Sprintf("%s:%d", host, p)
-		c, err := net.DialTimeout("tcp", addr, 2*time.Second)
-		if err != nil {
-			fmt.Printf("%-24s closed\n", addr)
-			continue
-		}
-		fmt.Printf("%-24s open\n", addr)
-		c.Close()
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+		fmt.Fprintf(w, "# HELP go_goroutines Number of running goroutines\n")
+		fmt.Fprintf(w, "# TYPE go_goroutines gauge\n")
+		fmt.Fprintf(w, "go_goroutines %d\n", runtime.NumGoroutine())
+		fmt.Fprintf(w, "# HELP go_mem_alloc_bytes Memory allocated\n")
+		fmt.Fprintf(w, "# TYPE go_mem_alloc_bytes gauge\n")
+		fmt.Fprintf(w, "go_mem_alloc_bytes %d\n", m.Alloc)
+		fmt.Fprintf(w, "# HELP go_uptime_seconds Uptime\n")
+		fmt.Fprintf(w, "# TYPE go_uptime_seconds counter\n")
+		fmt.Fprintf(w, "go_uptime_seconds %d\n", time.Since(startTime).Seconds())
+	})
+	fmt.Printf("prometheus exporter on :%s/metrics\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
+
+var startTime = time.Now()
